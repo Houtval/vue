fd77b3881005db56787a3b774b63c88b2ae27944
vistal <1979831865@qq.com>
2023-03-09 19:51:49 +0800

    new

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -3,7 +3,8 @@ import { ElMessage } from 'element-plus'
 //使用axios下面的create([config])方法创建axios实例，其中config参数为axios最基本的配置信息。
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 const Api = axios.create({
-	baseURL:'/api', //请求后端数据的基本地址，自定义
+	url:'/api',
+	baseURL:'http://127.0.0.1:9527', //请求后端数据的基本地址，自定义
 	timeout: 2000   //请求超时设置，单位ms
 })
 
